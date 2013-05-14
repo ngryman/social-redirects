6f3325870f8fd1a4ec436ebba01dfc0ff6b237ce
Hans Christian Reinl <info@drublic.de>
2013-05-14 23:05:54 +0200

    Add www for Facebook redirect
    Facebook redirects to www.facebook.com by default. By using the www
    subdomain it is possible to save an extra redirect.

--- webapp/app.js
+++ webapp/app.js
@@ -3,7 +3,7 @@
 		'/@': 'https://twitter.com/:you',
 		'/~': 'https://github.com/:you',
 		'/+': 'https://plus.google.com/:you',
-		'/f': 'https://facebook.com/:you',
+		'/f': 'https://www.facebook.com/:you',
 		'/in': 'https://linkedin.com/in/:you'
 	}[window.location.pathname]) && window.location.replace(url);
 })();
